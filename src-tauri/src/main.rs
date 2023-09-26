86581676fd5d05e701ae500420bfa2815017112f
daniel <dsbadal@gmail.com>
2023-09-26 08:32:24 -0700

    Add command to check if file is directory

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,32 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+use tauri::Manager;
+use std::env;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn is_dir(path: &str) -> bool {
+    let path_for_os = match env::consts::OS {
+        "windows" => path.to_string(),
+        _ => path.to_string()
+    };
+    let path_for_err = path_for_os.clone();
+    let is_dir_result = tauri::api::dir::is_dir(path_for_os);
+    match is_dir_result  {
+        Ok(result) => result,
+        Result::Err(error) => {
+            println!("Error checking is dir; {:?}; <{:?}> ", error, path_for_err);
+            false
+        }
+    }
 }
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![is_dir])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
